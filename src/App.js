9c4e3b9c2f32af58a95bf43d88a97c7c57079d51
Andrew Welsh <shimeroh@gmail.com>
2018-11-10 16:23:18 +1300

    Began adding api calls!

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import logo from './pokeball.svg';
 import './App.css';
 
+var Pokedex = require('pokedex-promise-v2');
+var P = new Pokedex();
+
+P.resource("https://pokeapi.co/api/v2/pokemon/1/").then(function(response)
+{
+  console.log(response.forms[0].name);
+  console.log(response.sprites.front_default);
+});
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <p id = "test">
             This is going to turn into a pokedex!
           </p>
         </header>
