6d006b57af799647f24432fc161aaebec42a51c5
Andrew Welsh <shimeroh@gmail.com>
2018-11-10 19:09:41 +1300

    Created a component to display a pokemon and it's name, also changed animation a tiny bit

--- src/App.js
+++ src/App.js
@@ -22,14 +22,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
           <img id ="sprite" src={logo} className="App-logo" alt="logo" />
           <p id = "test">
             This is going to turn into a pokedex!
           </p>
-        </header>
+          <Entry name="bulbasaur">
+          </Entry>
       </div>
     );
+  }
+}
+
+class Entry extends Component {
+  render() {
+    return (
+      <div classname="Entry">
+        <img id="sprite" src={this.props.imagesrc} className="App-logo" alt="logo" />
+        <p id="name">
+          {this.props.name}
+        </p>
+      </div>
+    )
   }
 }
 
